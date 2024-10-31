86de55817d63ec857e2f8e3835b05cf394e5d461
Hunter Johns <Hunter@whichfishstudios.com>
2024-10-31 15:36:47 +0000

    initial files

---
+++ test/test.js
@@ -0,0 +1,14 @@
+var main = require('../index');
+var assert = require('assert');
+describe('Regular fibonacci value', function() {
+    it('should return 120 for Fact(5)', function() {
+        assert.strictEqual(main.factorial(5), 120);
+      });
+});
+
+describe('First two terms', function() {
+  it('should return 0 and 1 for first two terms', function() {
+      assert.strictEqual(main.factorial(0), 0);
+      assert.strictEqual(main.factorial(1), 1);
+    });
+});
